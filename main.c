8dfcde41e45c1333d6a23ec8645e6be0ba63af98
Jery <>
2022-12-08 18:17:16 +0000

    Menu

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
+#include <locale.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Ayudaaaaa"); 
+int main() {
+  setlocale(LC_ALL, "spanish");
+  int num;
+  // Spotify UV
+  // Menu de inicio
+  printf("Spotify UV\n");
+  printf("[1] Explora Generos\n");
+  printf("[2] Explorar Canciones\n");
+  printf("[3] Reproducir\n");
+  printf("[4] Salir\n");
+
+  switch (num) {
+  case 1:
+    printf("[1] Explora Generos\n");
+    break;
+  case 2:
+    printf("[2] Explorar Canciones\n");
+    break;
+    case 3:
+    printf("[3] Reproducir\n");
+    break;
+    case 4:
+    
+  }
+
+  /*for(int i = 0; i < 10; i++)
+    printf("Ayudaaaaa\n"); */
+
   return 0;
 }
